4541d343f8b885ff42887d57555b77c355dfe4e6
anastazess <155468766+anastazess@users.noreply.github.com>
2026-08-19 16:39:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "8843372032:AAG4OzTYe4Wte1INNwRumIYaNKLiQvP0z6Q"
-ADMIN_IDS = [8205318702]  # Telegram ID администраторов
+BOT_TOKEN = ""
+ADMIN_IDS = []  # Telegram ID администраторов
 DB_PATH = "league.db"
